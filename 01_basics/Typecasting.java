c62108836ab4dc545bb3ae5335cc57caa1f03c45
bhaveshp <bhavesh.parmanand.peswani@ut.ee>
2025-08-05 21:12:29 +0530

    Typecasting(from 1 data type to other)

---
+++ 01_basics/Typecasting.java
@@ -0,0 +1,11 @@
+public class Typecasting {
+    public static void main(String[] args) {
+        int value = 10;
+        double doubleValue = value;
+        System.out.println("Integer value: " + value);
+        System.out.println("Double value after typecasting: " + doubleValue);
+
+    }
+
+    
+}
