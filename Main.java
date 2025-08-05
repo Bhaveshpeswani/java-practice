4909846cd3baea01ef2a5b07f54c4030150412ae
bhaveshp <bhavesh.parmanand.peswani@ut.ee>
2025-08-05 15:23:17 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main{
+    public static void main(String args[]) {
+        System.out.println("Hello bhavesh");
+        System.out.println("we are learning java");
+        System.out.println(100 +  "%");
+    }
+}
