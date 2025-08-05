0515c06dabf9dba88bf3c32841470c1256d1e91f
bhaveshp <bhavesh.parmanand.peswani@ut.ee>
2025-08-05 20:25:01 +0530

    Variable creation

---
+++ 01_basics/Variable.java
@@ -0,0 +1,14 @@
+public class Variable {
+    public static void main(String[] args) {
+        int number  = 10;
+        float decimal = 10.5f;
+        char letter = 'A';
+        String name = "Bhavesh";
+        boolean myValue = true;
+        System.out.println("Number: " + number );
+        System.out.println("Name: " + name);
+        System.out.println("Decimal: " + decimal);
+        System.out.println("Letter: " + letter);
+        System.out.println("myValue: " + myValue);
+    }
+}
