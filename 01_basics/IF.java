9085c9f4d9e4f5747c89fc0ddd4e262a0d214c0c
bhaveshp <bhavesh.parmanand.peswani@ut.ee>
2025-08-06 19:52:25 +0530

    Short Hand If...Else expression

---
+++ 01_basics/IF.java
@@ -0,0 +1,12 @@
+
+public class IF {
+  public static void main(String[] args) {
+    int time = 20;
+    String result;
+    result = (time < 19) ? "Good day." : "Good evening.";
+    System.out.println(result);
+  }
+}
+
+
+
