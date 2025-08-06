57d375c9cdbe30fa24b1f4a93bc1923d094266cb
bhaveshp <bhavesh.parmanand.peswani@ut.ee>
2025-08-06 19:57:25 +0530

    Java if else real life( odd  and even)

--- 01_basics/IF.java
+++ 01_basics/IF.java
@@ -1,12 +1,11 @@
-
 public class IF {
-  public static void main(String[] args) {
-    int time = 20;
-    String result;
-    result = (time < 19) ? "Good day." : "Good evening.";
-    System.out.println(result);
-  }
-}
+    public static void main(String[] args) {
+       int num = 10 ;
 
-
-
+       if( num % 2 == 0 ){
+        System.out.println(" it's even " + num);
+       } else {
+        System.out.println(" it's odd " + num);
+       }
+       }
+    }
