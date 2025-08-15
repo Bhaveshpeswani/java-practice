87bcf641134e5130c77630af0b65c8661462cc05
bhaveshp <bhavesh.parmanand.peswani@ut.ee>
2025-08-15 11:21:10 +0530

    For Each example

---
+++ 01_basics/ForEach.java
@@ -0,0 +1,9 @@
+public class ForEach {
+    
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "Tesla", "BMW", "Ferrari"};
+        for (String car : cars){
+            System.out.println(car);
+        }
+    }
+}
