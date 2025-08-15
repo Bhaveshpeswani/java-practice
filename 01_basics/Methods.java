7256909551a53d39a03165f925d0c99b76aa0c62
bhaveshp <bhavesh.parmanand.peswani@ut.ee>
2025-08-15 18:12:50 +0530

    Methods Example

---
+++ 01_basics/Methods.java
@@ -0,0 +1,11 @@
+public class Methods {
+
+
+     static void MyMethod(){
+            System.out.println("Hey This is your First Method, welcome to Java!");
+        }
+
+    public static void main(String args[]) {
+        MyMethod();
+    }
+}
