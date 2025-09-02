1aaa8e0ca1957bd104ebf57fa632d889f1d967bf
bhaveshp <bhavesh.parmanand.peswani@ut.ee>
2025-09-02 17:29:02 +0300

    Oops Using Multiple Classes

--- 02_classes/Main.java
+++ 02_classes/Main.java
@@ -1,12 +1,10 @@
 public class Main {
-    int x = 10;
+    int x = 5;
 
-    public static void main(String[] args) {
-        Main MyObj1 = new Main();
-        Main MyObj2 = new Main();
-
-        System.out.println(MyObj1.x);
-        System.out.println(MyObj2.x);
-
+    static class Second {
+        public static void main(String[] args) {
+            Main myObj = new Main();
+            System.out.println(myObj.x);
+        }
     }
 }
