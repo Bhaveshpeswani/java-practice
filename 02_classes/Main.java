2b84c1d565da73049b95241021a4edcea5d912f1
bhaveshp <bhavesh.parmanand.peswani@ut.ee>
2025-09-02 15:31:47 +0300

    Oops basic example

---
+++ 02_classes/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    int x = 10;
+
+    public static void main(String[] args) {
+        Main MyObj1 = new Main();
+        Main MyObj2 = new Main();
+
+        System.out.println(MyObj1.x);
+        System.out.println(MyObj2.x);
+
+    }
+}
